deb0da93c7316cc5e72afa7de8ddeba3bfd55fc6
manuK1777 <manuelkrapovickas@gmail.com>
2025-02-05 19:44:16 +0100

    Fix h1 responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -69,22 +69,18 @@ export default function Home() {
 
         {/* Content */}
         <motion.div
-          className="relative z-10 flex items-end justify-center min-h-screen pb-10 sm:pb-8 xs:pb-10 lg:pb-0"
+          className="relative z-10 flex items-end justify-center min-h-screen pb-[3.5rem] sm:pb-8 lg:pb-[0]"
           initial={{ y: 100, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 1, delay: 1.2 }}
         >
           <h1
-            className="text-[3.5rem] text-[4.2rem] xs:text-[4.5rem] sm:text-[5.7rem] md:text-[5.8rem] lg:text-[7rem] font-[930] 
+            className="text-[3rem] sm:text-[4.3rem] md:text-[4.5rem] lg:text-[6.5rem] font-[930] 
              text-red-600 text-center px-4 leading-none font-[Arial_Black,Impact,sans-serif]"
           >
             <span className="block">The Extended</span>
-            <span
-              className="block -mt-[1rem] -mt-[1.2rem] xs:-mt-[1rem] sm:-mt-[1.7rem] md:-mt-[1.8rem] lg:-mt-[2.1rem] 
-                         ml-[0.8rem] xs:ml-[0.6rem] sm:ml-[0.8rem] md:ml-[1rem] lg:ml-[1rem]"
-            >
-              Bandoneon
-            </span>
+            <span className="block -mt-[1rem] -mt-[1.2rem] sm:-mt-[1.2rem] md:-mt-[1.5rem] lg:-mt-[1.9rem] 
+                         ml-[0.8rem] xs:ml-[0.6rem] sm:ml-[0.8rem] md:ml-[0.8rem] lg:ml-[1rem]">Bandoneon</span>
           </h1>
         </motion.div>
       </div>
