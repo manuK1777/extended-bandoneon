7a41034c62a36d9d2576a3dfe4d20821f085182b
manuK1777 <manuelkrapovickas@gmail.com>
2025-02-06 10:39:14 +0100

    Fix h1 position for small devices

--- app/page.tsx
+++ app/page.tsx
@@ -69,17 +69,18 @@ export default function Home() {
 
         {/* Content */}
         <motion.div
-          className="relative z-10 flex items-end justify-center min-h-screen pb-[6.5rem] sm:pb-[6rem] md:pb-[6rem] lg:pb-[0]"
+          className="relative z-10 flex items-end justify-center min-h-screen pb-[4rem] sm:pb-[6rem] md:pb-[6rem] lg:pb-[0]"
           initial={{ y: 100, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 1, delay: 1.2 }}
         >
           <h1
-            className="text-[3rem] sm:text-[4.3rem] md:text-[4.5rem] lg:text-[6.5rem] font-[930] 
+            className="text-[4rem] sm:text-[4.3rem] md:text-[4.5rem] lg:text-[6.5rem] font-[930] 
              text-red-600 text-center px-4 leading-none font-[Arial_Black,Impact,sans-serif]"
           >
-            <span className="block">The Extended</span>
-            <span className="block -mt-[1rem] -mt-[1.2rem] sm:-mt-[1.2rem] md:-mt-[1.5rem] lg:-mt-[1.9rem] 
+            <span className="block sm:inline">The </span>
+            <span className="block sm:inline -mt-[0.9rem]">Extended</span>
+            <span className="block -mt-[0.5rem] sm:-mt-[1.2rem] md:-mt-[1.5rem] lg:-mt-[1.9rem] 
                          ml-[0.8rem] xs:ml-[0.6rem] sm:ml-[0.8rem] md:ml-[0.8rem] lg:ml-[1rem]">Bandoneon</span>
           </h1>
         </motion.div>
