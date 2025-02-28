22ddf3e45a92b458c49ceecb2640a760b4c2fa60
manuK1777 <manuelkrapovickas@gmail.com>
2025-02-28 12:20:18 +0100

    Fix soundplayer colour styles, fuchsia

--- components/SoundPlayer.tsx
+++ components/SoundPlayer.tsx
@@ -81,18 +81,18 @@ export default function SoundPlayer({ fileUrl, onReady }: SoundPlayerProps) {
       ) : (
         <div>
           <div ref={containerRef} className="w-full rounded-lg" />
-          <div className="flex items-center justify-between text-sm text-fuchsia-500">
+          <div className="flex items-center justify-between text-sm text-fuchsia-200">
             <button
               onClick={togglePlayPause}
               className="flex items-center space-x-2 hover:text-yellow-200 transition-colors"
             >
               <span className="sr-only">{isPlaying ? 'Pause' : 'Play'}</span>
               {isPlaying ? (
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-yellow-200">
                   <path fillRule="evenodd" d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7 0a.75.75 0 01.75-.75h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75h-1.5a.75.75 0 01-.75-.75V5.25z" clipRule="evenodd" />
                 </svg>
               ) : (
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-fuchsia-400">
                   <path fillRule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clipRule="evenodd" />
                 </svg>
               )}
