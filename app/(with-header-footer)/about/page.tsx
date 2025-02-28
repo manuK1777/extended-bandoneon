d6fb490c530e3ac0c5929243f1b607f0cbc6d37b
manuK1777 <manuelkrapovickas@gmail.com>
2025-02-28 11:53:15 +0100

    Fix: escape about page single quotes using &apos, fix text vertical spacing

--- app/(with-header-footer)/about/page.tsx
+++ app/(with-header-footer)/about/page.tsx
@@ -6,7 +6,7 @@ export default function AboutPage() {
         <h1 className="text-3xl md:text-4xl font-bold mb-8 text-yellow-200 font-heading">About</h1>
         <div className="text-gray-300 font-body text-lg leading-relaxed">
           <p className="mb-4">
-            Welcome! I am Mercedes Krapovickas, a bandoneonist, pianist, composer, and sound artist from Argentina, based in Finland. My work explores the bandoneon's movement, textures, and expanded possibilities through extended techniques, sound design, and electronics.
+            Welcome! I am Mercedes Krapovickas, a bandoneonist, pianist, composer, and sound artist from Argentina, based in Finland. My work explores the bandoneon&apos;s movement, textures, and expanded possibilities through extended techniques, sound design, and electronics.
           </p>
           <p className="mb-4">
             This platform is dedicated to sharing resources sound banks, extended techniques, and, in the future, a software tool for bandoneon augmentation. My goal is to inspire musicians and sound artists to reimagine the instrument beyond its traditional role.
@@ -18,19 +18,19 @@ export default function AboutPage() {
       <div className="space-y-12">
         <section>
           <h2 className="text-2xl font-display mb-4 text-fuchsia-500">Research & Background</h2>
-          <div className="text-gray-300 font-body space-y-4">
+          <div className="text-gray-300 font-body space-y-4 leading-relaxed">
             <p>
-              I am currently pursuing a doctorate at the Sibelius Academy's Music and Technology Department, where my research, Bandoneon Resonances, examines movement, interaction, and augmentation. I also study avant-garde bandoneon compositions from the 1960s.
+              I am currently pursuing a doctorate at the Sibelius Academy&apos;s Music and Technology Department, where my research, Bandoneon Resonances, examines movement, interaction, and augmentation. I also study avant-garde bandoneon compositions from the 1960s.
             </p>
             <p>
-              Beyond research, I have performed across Europe and Argentina, both as a soloist and in interdisciplinary projects, collaborating in dance, theater, and circus productions. My background includes a Master's in Sound in New Media from Aalto University and studies in electroacoustic composition, musicology, piano, and composition in Argentina and Finland.
+              Beyond research, I have performed across Europe and Argentina, both as a soloist and in interdisciplinary projects, collaborating in dance, theater, and circus productions. My background includes a Master&apos;s in Sound in New Media from Aalto University and studies in electroacoustic composition, musicology, piano, and composition in Argentina and Finland.
             </p>
           </div>
         </section>
 
         <section>
           <h2 className="text-2xl font-display mb-4 text-fuchsia-500">Vision</h2>
-          <div className="text-gray-300 font-body space-y-4">
+          <div className="text-gray-300 font-body space-y-4 leading-relaxed">
             <p>
               This website is a space for experimentation and exploration for those curious about reimagining the bandoneon!
             </p>
@@ -39,7 +39,7 @@ export default function AboutPage() {
 
         <section>
           <h2 className="text-2xl font-display mb-4 text-fuchsia-500">Acknowledgments</h2>
-          <div className="text-gray-300 font-body space-y-4">
+          <div className="text-gray-300 font-body space-y-4 leading-relaxed">
             <p>
               I would like to express my gratitude to those who have supported and contributed to this project:
             </p>
